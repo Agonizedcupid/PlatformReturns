38c97d9c9f56117dbf22eb0f5b6680968fa3b3ec
Agonizedcupid <agonizedcupid@gmail.com>
2022-05-28 22:51:46 +0600

    Home design added

--- app/src/main/java/com/aariyan/platformreturns/Home.java
+++ app/src/main/java/com/aariyan/platformreturns/Home.java
@@ -2,13 +2,102 @@ package com.aariyan.platformreturns;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Home extends AppCompatActivity {
 
+    private TextView getCustomerBtn, dateSelector;
+    private Spinner routeSelector;
+
+    private DatePickerDialog datePickerDialog;
+    private Calendar calendar;
+    int day, month, year;
+    String date = "";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setTitle("Home");
+
+        calendar = Calendar.getInstance();
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        month = calendar.get(Calendar.MONTH);
+        year = calendar.get(Calendar.YEAR);
+
+        initUI();
     }
+
+    private void initUI() {
+        getCustomerBtn = findViewById(R.id.getCustomerBtn);
+        dateSelector = findViewById(R.id.dateSelector);
+        routeSelector = findViewById(R.id.routeSelector);
+        
+        getCustomerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+        
+        dateSelector.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectDate();
+            }
+        });
+    }
+
+    private void selectDate() {
+        datePickerDialog = new DatePickerDialog(Home.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+
+                //Month
+                int j = i1 + 1;
+
+                //date = i + "-" + j + "-" + i2;
+                //date = i2 + "-" + j + "-" + i;
+                date = i + "-" + j + "-" + i2;
+                //2022-1-15
+                dateSelector.setText(date);
+
+            }
+            //}, day, month, year);
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+        //datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+//        new DatePickerDialog(AddTimeActivity.this, null, calendar.get(Calendar.YEAR),
+//                calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+
+        datePickerDialog.show();
+    }
+
+//    private void routeSpinnerFunc(List<RouteModel> routeList) {
+//
+//        //Spinner items
+//        ArrayAdapter<RouteModel> dataAdapter = new ArrayAdapter<RouteModel>(Home.this,
+//                android.R.layout.simple_spinner_item, routeList);
+//        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        routeSpinner.setAdapter(dataAdapter);
+//        routeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+//                //selectedRoute = Integer.parseInt(adapterView.getItemAtPosition(position).toString());
+//                selectedRoute = routeList.get(position).getRouteid();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//    }
 }
